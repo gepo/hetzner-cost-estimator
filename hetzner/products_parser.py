b7a31fb63ad803e8558258a7bdb4580fda94cc39
Gennady Telegin <gtelegin@gmail.com>
2017-06-30 00:09:42 +0300

    fix prices for dedicated servers after new layout.
    TODO: prices for vserver,managed,box and flexipack.

--- hetzner/products_parser.py
+++ hetzner/products_parser.py
@@ -13,38 +13,21 @@ def _parseServerProducts():
     soup = BeautifulSoup(resp.text, 'html.parser')
 
     map = {
-        'dedicated': _parseDedicated(soup),
+        'dedicated': _parseDedicated(),
         'vserver': _parseVserver(soup),
         'managed': _parseManaged(soup),
         'box': _parseBox(soup),
     }
-
     return map
 
-def _parseDedicated(soup):
-    map = {}
+def _parseDedicated():
+    url = 'https://www.hetzner.de/dedicated-rootserver/getServer'
+    data = requests.get(url).json()
 
-    for item_list in soup.find_all('li', class_='tded')[0].find_all('ul'):
-        for item in item_list.find_all('li'):
-
-            a_item = item.find('a')
-            if a_item is None:
-                continue
-
-            product_name = a_item.contents[0].rstrip(' ')
-            price_elem = a_item.find('font', class_='klapp_preise')
-            if price_elem is None:
-                continue
-
-            price_elem = price_elem.find('noscript')
-            if price_elem is None:
-                continue
-
-            map[product_name] = float(price_elem.text.replace(',', '.'))
-
-    return map
+    return { srv['name']: float(srv['price_v']) for srv in data['server'] }
 
 def _parseVserver(soup):
+    return {} # FIXME
     map = {}
 
     for item in soup.find_all('li', class_='tded')[1].find_all('li'):
@@ -68,6 +51,7 @@ def _parseVserver(soup):
     return map
 
 def _parseManaged(soup):
+    return {} # FIXME
     map = {}
 
     for item in soup.find_all('li', class_='mded')[0].find_all('li'):
@@ -91,6 +75,7 @@ def _parseManaged(soup):
     return map
 
 def _parseBox(soup):
+    return {} # FIXME
     map = {}
 
     for item in soup.find_all('li', class_='support')[0].find_all('ul')[0].find_all('li'):
@@ -114,6 +99,7 @@ def _parseBox(soup):
     return map
 
 def _parseFlexiPack():
+    return {} # FIXME
     resp = requests.get('https://www.hetzner.de/de/hosting/produkte_rootserver/flexipack')
     soup = BeautifulSoup(resp.text, 'html.parser')
 
